17d7437f6426fd023d234aaa603bc47b6c23a6e7
Gabriel <gamejia1202@gmail.com>
2024-08-20 19:38:14 -0600

    Primer Commit Calculadora

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,10 @@
+package calculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        VentanaC ventana = new VentanaC();
+        ventana.setVisible(true);
+    }
+
+}
